265c8a8d8dc61760186b0b4721d18cc79501e670
Jennifer <142938327+Jennifercruz23@users.noreply.github.com>
2025-08-24 09:57:17 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/fulltextsearch/main.go
@@ -0,0 +1,23 @@
+// cmd/fulltextsearch/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "fulltextsearch/internal/fulltextsearch"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := fulltextsearch.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
